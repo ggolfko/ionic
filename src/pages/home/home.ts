91389e0babbe74dc032844ba28ddf248448d9fb3
Kitti Onthong <5535512134@email.psu.ac.th>
2017-05-28 02:52:33 +0700

    add feature Building a Users App with Ionic 2, MongoDB & Node

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+
+import { UserDataProvider } from '../../providers/user-data/user-data';
+import { AddUserPage } from '../add-user/add-user';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: any;
 
+  constructor(public navCtrl: NavController,
+    public userService: UserDataProvider,
+    public modalCtrl: ModalController) {
+
+  }
+
+  ionViewDidLoad() {
+
+    this.userService.loadUser().then(data => {
+      this.users = data;
+    }, err => {
+
+    });
+
+  }
+
+  addUser() {
+
+    let modal = this.modalCtrl.create(AddUserPage);
+
+    modal.onDidDismiss(data => {
+      if (data) {
+        this.users.push(data);
+        this.userService.createUser(data);
+      }
+    });
+
+    modal.present();
+
+  }
+
+  deleteUser(data) {
+
+    //Remove locally
+    let index = this.users.indexOf(data);
+
+    if (index > -1) {
+      this.users.splice(index, 1);
+    }
+
+    //Remove from database
+    this.userService.deleteUser(data._id);
   }
 
 }
