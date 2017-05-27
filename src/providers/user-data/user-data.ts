a1ea1858f3f33ddcaca2ea83d88304ea82d2fa1d
Kitti Onthong <5535512134@email.psu.ac.th>
2017-05-28 02:53:54 +0700

    add provider user-data

---
+++ src/providers/user-data/user-data.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the UserDataProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UserDataProvider {
+
+  data: any;
+
+  constructor(public http: Http) {
+    console.log('Hello UserDataProvider Provider');
+  }
+
+  // get all the use
+  loadUser() {
+
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      // We're using Angular HTTP provider to request the data,
+      // then on the response, it'll map the JSON data to a parsed JS object.
+      // Next, we process the data and resolve the promise with the new data.
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+
+      this.http.get('http://localhost:8080/api/users', options)
+        .map(res => res.json())
+        .subscribe(data => {
+          // we've got back the raw data, now generate the core schedule data
+          // and save the data for later reference
+          this.data = data;
+          resolve(this.data);
+        });
+    });
+
+  }
+
+  // create a User
+  createUser(data) {
+    let params = JSON.stringify(data);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:8080/api/users', params, options)
+      .subscribe(res => {
+        console.log(res.json());
+      });
+
+  }
+
+  // delete the user with this id
+  deleteUser(id) {
+
+    return this.http.delete('http://localhost:8080/api/users/' + id).subscribe((res) => {
+      console.log(res.json());
+    });
+
+  }
+
+}
